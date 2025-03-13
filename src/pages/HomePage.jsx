cd10b7bededef13a11d0f6f3bb8ebb3d91c4a192
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-03-13 16:26:43 +0600

    step section design formation created

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FeaturesSection from "./homeComponents/FeaturesSection";
+import StepsSection from "./homeComponents/StepsSection";
 
 
 const HomePage = () => {
   return <>
-  <div>HomePage</div>
-  <div><FeaturesSection></FeaturesSection></div>
+    <div>HomePage</div>
+    <div><FeaturesSection></FeaturesSection></div>
+    <div><StepsSection></StepsSection></div>
   </>;
 };
 
