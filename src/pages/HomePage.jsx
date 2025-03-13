1f37ca0c991355caafb931e7a0d10d7605eaaf71
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-03-13 20:14:29 +0600

    make home page by defult and make it ready for pull request

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,7 @@
 import React from "react";
-import FeaturesSection from "./homeComponents/FeaturesSection";
-import StepsSection from "./homeComponents/StepsSection";
-
 
 const HomePage = () => {
-  return <>
-    <div>HomePage</div>
-
-
-    {/* feacher section import */}
-    <div><FeaturesSection></FeaturesSection></div>
-
-    {/* step by step section added */}
-    <div><StepsSection></StepsSection></div>
-  </>;
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
