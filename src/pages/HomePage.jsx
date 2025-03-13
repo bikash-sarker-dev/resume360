e446318c417130384610fcf9ce6f370ce1e927cc
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-03-13 17:55:13 +0600

    in the home page comment are added and project are done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,12 @@ import StepsSection from "./homeComponents/StepsSection";
 const HomePage = () => {
   return <>
     <div>HomePage</div>
+
+
+    {/* feacher section import */}
     <div><FeaturesSection></FeaturesSection></div>
+
+    {/* step by step section added */}
     <div><StepsSection></StepsSection></div>
   </>;
 };
