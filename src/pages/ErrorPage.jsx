ccd1a5cc245431a5354c379b7e07dd686acc40ab
Sadia0920 <sadiaafrin0920@gmail.com>
2025-03-14 14:24:06 +0600

    added an error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return <div>
+  <div className='pb-10 my-7 text-center bg-[rgb(134,171,223,0.5)] text-[rgb(51,51,51)] w-10/12 mx-auto rounded-xl'>
+    <div className="size-[500px] mx-auto">
+    <Lottie animationData={errorLottieData}></Lottie>
+    </div>
+    <p className="text-5xl mb-4">Pages are not found</p>
+    <p className='text-lg font-semibold'>Please go to - <Link to='/' className='border-b-2 text-[#6d81d9] border-[#6d81d9]'>Home</Link></p>
+  </div>
+  </div>;
 };
 
 export default ErrorPage;
