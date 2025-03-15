4e6a5db3b17e2732eec884f9decde1c185dc3fbb
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-03-16 01:19:57 +0600

    the some route create in application

--- src/routes/RootRoute.jsx
+++ src/routes/RootRoute.jsx
@@ -5,7 +5,11 @@ import MainLayout from "../layouts/MainLayout";
 import ErrorPage from "../pages/ErrorPage";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
+import PersonalInfoPage from "../pages/PersonalInfoPage";
 import RegisterPage from "../pages/RegisterPage";
+import TemplatePage from "../pages/TemplatePage";
+import User from "../pages/User";
+import AboutPage from "./../pages/AboutPage";
 
 const RootRoute = () => {
   return (
@@ -14,6 +18,10 @@ const RootRoute = () => {
         <Route index element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/templates" element={<TemplatePage />} />
+        <Route path="/personal-info" element={<PersonalInfoPage />} />
       </Route>
       <Route path="*" element={<ErrorPage />} />
     </Routes>
