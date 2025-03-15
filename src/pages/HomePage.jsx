948107ef29b60fef0a170a373d5449e51210f82f
kazi tareq <kazitareq321@gmail.com>
2025-03-16 01:18:40 +0600

    update some design navbar and hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Banner from "../components/home/Banner/Banner";
+// import Banner from "../components/home/Banner/Banner";
 import { Blogs } from "../components/home/Blogs/Blogs";
 import BrandLogo from "../components/home/brandLogo/BrandLogo";
 import Counter from "../components/home/counter/Counter";
@@ -8,6 +8,7 @@ import { Subscriptions } from "../components/home/Subscriptions/Subscriptions";
 
 import FeaturesSection from "./../components/home/features/FeaturesSection";
 import StepsSection from "./../components/home/stepsBySteps/StepsSection";
+import Banner from "../components/Banner/Banner";
 
 const HomePage = () => {
   return (
