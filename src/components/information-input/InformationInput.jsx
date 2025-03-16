b24a03d6352efac535104dba3f1726f458f4fe43
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-03-17 01:15:14 +0600

    stepper components added i hold this design and move to personal information page

---
+++ src/components/information-input/InformationInput.jsx
@@ -0,0 +1,15 @@
+import StepperComponents from "./stepper/StepperComponents";
+
+const InformationInput = () => {
+    return (
+        <>
+            <div className="h-screen">
+                <div className="max-w-7xl mx-auto pt-30">
+                    <StepperComponents></StepperComponents>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default InformationInput;
