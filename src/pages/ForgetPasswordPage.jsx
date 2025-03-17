4ab1e45ef2e81970a01cbc1750c718c24d445947
Sadia0920 <sadiaafrin0920@gmail.com>
2025-03-18 01:46:36 +0600

    setup firebase

--- src/pages/ForgetPasswordPage.jsx
+++ src/pages/ForgetPasswordPage.jsx
@@ -1,27 +1,18 @@
 import React, { useState } from "react";
 import Lottie from "lottie-react";
 import forgetPasswordLottieData from "../assets/animation/forget-password.json"
-import { Link } from "react-router";
+import ForgetPassword from "../auth/forgetPassword/ForgetPassword";
 
 const ForgetPasswordPage = () => {
   return <div className="container pb-8 pt-24 lg:pb-12">
   <div className="hero bg-background">
  <div className="hero-content flex-1 flex-col-reverse lg:flex-row gap-10">
-
    <div className="card w-full md:w-9/12 lg:w-6/12 shadow-2xl">
      <div className="card-body">
        <h2 className="text-xl lg:text-2xl font-bold">Forget Password</h2>
        <p className="w-full mx-auto lg:mx-0 my-2">
         Enter your email address below to receive a passowrd reset link.</p>
-      <form>
-      <fieldset className="fieldset">
-         <label className="fieldset-label">Email</label>
-         <input type="email" name="email" className="input w-full" placeholder="Enter Your Email" />
-         <button className="btn bg-r-accent mt-4 text-white">Send Reset Email</button>
-       </fieldset>
-        <p className="text-center mt-4">Remember Your Password? <span className="underline"><Link to='/login' className="text-r-accent">Login</Link></span>
-             </p>
-      </form>
+     <ForgetPassword></ForgetPassword>
      </div>
    </div>
    <div className="w-9/12 md:w-7/12 lg:w-6/12">
