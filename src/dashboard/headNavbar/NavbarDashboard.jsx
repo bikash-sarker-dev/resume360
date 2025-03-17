da813d7758b501eeb91af509c19b218338db2eff
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-03-18 02:13:04 +0600

    dashboard top navbar markup done

---
+++ src/dashboard/headNavbar/NavbarDashboard.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const NavbarDashboard = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" required placeholder="Search" />
+          </label>
+        </div>
+        <div>
+          <div className="indicator">
+            <span className="indicator-item badge badge-secondary">12</span>
+            <button className="btn">inbox</button>
+          </div>
+        </div>
+        <div>
+          <h4>user name</h4>
+          <div className="avatar">
+            <div className="w-24 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarDashboard;
