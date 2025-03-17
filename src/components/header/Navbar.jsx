65c7b660221de339b4d132ba1734e961f68ee7bc
Sadia0920 <sadiaafrin0920@gmail.com>
2025-03-18 03:25:21 +0600

    setup signout funcationality to the navbar and added logout button

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,11 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2";
 
 const Navbar = () => {
+  const {signOutUser,user,setUser} = useAuth()
+  const navigate = useNavigate()
   const [menuOpen, setMenuOpen] = useState(false);
   const [lastScroll, setLastScroll] = useState(0);
   const [hidden, setHidden] = useState(false);
+
+  // Signout
+  const handleSignOut = () => {
+    // console.log('logOut')
+    signOutUser()
+    .then(()=>{
+      Swal.fire({
+        title: 'Success',
+        text: 'Logout successfully',
+        icon: 'success',
+        confirmButtonText: 'Done'
+      })
+      setUser(null)
+    })
+    .catch(error => {
+    })
+  }
 
   // Handle scroll behavior (hide on scroll down, show on scroll up)
   useEffect(() => {
@@ -60,20 +81,32 @@ const Navbar = () => {
           <ul className="hidden md:flex ml-20  space-x-6  text-lg font-medium">
             {menu}
           </ul>
-
-          {/* Login Button (Desktop) */}
-          <div className="flex gap-4">
-          <Link to="/login">
+          {
+            user?
+            <>
+            <Link>
+            <button onClick={handleSignOut} className="hidden lg:block shadow shadow-r-primary px-6 py-2 rounded-lg text-lg font-semibold duration-500 text-r-accent hover:text-r-text hover:bg-r-primary transition">
+              Logout
+            </button>
+            </Link>
+            </>
+            :
+            <>
+            {/* Login Button (Desktop) */}
+            <div className="flex gap-4">
+            <Link to="/login">
             <button className="hidden lg:block shadow shadow-r-primary px-6 py-2 rounded-lg text-lg font-semibold duration-500 text-r-accent hover:text-r-text hover:bg-r-primary transition">
               Login
             </button>
-          </Link>
-          <Link to="/register">
+            </Link>
+            <Link to="/register">
             <button className="hidden lg:block shadow shadow-r-primary px-6 py-2 rounded-lg text-lg font-semibold duration-500 text-r-accent hover:text-r-text hover:bg-r-primary transition">
-              register
+            register
             </button>
-          </Link>
-          </div>
+            </Link>
+            </div>
+            </>
+          }
 
           {/* Mobile hamburger Button */}
           <button
