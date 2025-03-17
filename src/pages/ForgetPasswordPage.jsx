f5799d0d579a7529f868856f4657f81bb6cf1eca
Sadia0920 <sadiaafrin0920@gmail.com>
2025-03-17 14:24:09 +0600

    linked up with login page

--- src/pages/ForgetPasswordPage.jsx
+++ src/pages/ForgetPasswordPage.jsx
@@ -17,7 +17,8 @@ const ForgetPasswordPage = () => {
          <input type="email" name="email" className="input w-full" placeholder="Enter Your Email" />
          <button className="btn bg-r-accent mt-4 text-white">Send Reset Email</button>
        </fieldset>
-        
+        <p className="text-center mt-4">Remember Your Password? <span className="underline"><Link to='/login' className="text-r-accent">Login</Link></span>
+             </p>
       </form>
      </div>
    </div>
