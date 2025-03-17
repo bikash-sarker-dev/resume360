9349881ab9f60b595d6425b32cea2f56463df968
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-03-17 23:14:07 +0600

    update code 17-03-2025

---
+++ src/layouts/DashboardLayouts.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidebar from "../dashboard/sideBar/Sidebar";
+
+const DashboardLayouts = () => {
+  return (
+    <main>
+      <div className="grid grid-cols-12">
+        <div className="col-span-2">
+          <Sidebar />
+        </div>
+        <div className="col-span-10">right side</div>
+      </div>
+    </main>
+  );
+};
+
+export default DashboardLayouts;
