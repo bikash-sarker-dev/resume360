ea6bd4ed8ab7b2a396cbc06f7b1af5f7146616c5
Sadia0920 <sadiaafrin0920@gmail.com>
2025-03-17 14:33:06 +0600

    added a faq route to the navbar

--- src/pages/FaqPage.jsx
+++ src/pages/FaqPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const FaqPage = () => {
-  return <div>FaqPage</div>;
+  return <div className="container py-8 lg:py-12">
+  <p className="text-center">
+  Frequesntly Asked Questions</p>
+  <h2 className="text-2xl lg:text-3xl font-bold text-center mt-3 mb-5">FAQ</h2>
+  <div className="divider"></div>
+</div>
 };
 
 export default FaqPage;
