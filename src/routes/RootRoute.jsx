f1aedfa91b3bc4c068795554ede265c20174494f
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-03-18 00:46:22 +0600

    added react-tab for showing different category template

--- src/routes/RootRoute.jsx
+++ src/routes/RootRoute.jsx
@@ -5,6 +5,7 @@ import MainLayout from "../layouts/MainLayout";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
+import TemplatesPage from "../pages/TemplatesPage";
 
 const RootRoute = () => {
   return (
@@ -13,6 +14,7 @@ const RootRoute = () => {
         <Route index element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/templates" element={<TemplatesPage></TemplatesPage>} />
       </Route>
     </Routes>
   );
