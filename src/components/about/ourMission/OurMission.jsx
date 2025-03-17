8dbd253671b67a948c565487ce23e143da3d3eb7
fhanif16 <fatemahanif16@gmail.com>
2025-03-17 11:49:31 -0400

    made responsive our team section

--- src/components/about/ourMission/OurMission.jsx
+++ src/components/about/ourMission/OurMission.jsx
@@ -1,35 +1,33 @@
+
+
 import React from 'react';
 
 const OurMission = () => {
     return (
-       <div className="container mx-auto py-16">
-         <div className='grid grid-cols-12 gap-4 mx-auto py-16'>
+       <div className="container mx-auto py-16 px-4">
+         <div className='grid grid-cols-1 md:grid-cols-12 gap-4 mx-auto'>
 
-<div className='grid lg:col-span-9 gap-8'>
+            <div className='md:col-span-9 space-y-6'>
+                <h1 className='text-2xl font-bold'>Our Mission</h1>
+                <h3 className='text-lg'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto totam, ut porro quis repellendus alias beatae deserunt cumque labore libero aliquid fugiat fuga eos ratione sapiente natus vel, soluta similique?</h3>
+                <h4 className='text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur natus accusamus voluptate, nam labore, aut praesentium veritatis voluptatum iusto doloribus explicabo tenetur aspernatur? Accusamus fugiat perferendis totam quaerat sequi fuga!</h4>
+            </div>
 
-    <h1>Our Mission</h1>
-    <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto totam, ut porro quis repellendus alias beatae deserunt cumque labore libero aliquid fugiat fuga eos ratione sapiente natus vel, soluta similique?</h3>
-
-
-    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur natus accusamus voluptate, nam labore, aut praesentium veritatis voluptatum iusto doloribus explicabo tenetur aspernatur? Accusamus fugiat perferendis totam quaerat sequi fuga!</h4>
-
-    
-
-</div>
-
-<div className='grid lg:col-span-3'>
-    <h1>44 million</h1> 
-    <p>Transactions every 24 hours</p>
-    <br></br>
-    <h1>44 million</h1> 
-    <p>Transactions every 24 hours</p>
-    <br></br>
-    <h1>44 million</h1> 
-    <p>Transactions every 24 hours</p>
-
-</div>
-
-</div>
+            <div className='md:col-span-3 flex flex-col space-y-6'>
+                <div>
+                    <h1 className='text-xl font-semibold'>44 million</h1> 
+                    <p>Transactions every 24 hours</p>
+                </div>
+                <div>
+                    <h1 className='text-xl font-semibold'>1.5 billion</h1> 
+                    <p>Processed annually</p>
+                </div>
+                <div>
+                    <h1 className='text-xl font-semibold'>99.9%</h1> 
+                    <p>Uptime guarantee</p>
+                </div>
+            </div>
+        </div>
        </div>
     );
 };
