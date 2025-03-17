5e17d9b60923c25a0044b5cc37a712abd0284fd6
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-03-18 01:54:19 +0600

    the dashboard left sidebar design

--- src/layouts/DashboardLayouts.jsx
+++ src/layouts/DashboardLayouts.jsx
@@ -4,11 +4,16 @@ import Sidebar from "../dashboard/sideBar/Sidebar";
 const DashboardLayouts = () => {
   return (
     <main>
-      <div className="grid grid-cols-12">
-        <div className="col-span-2">
+      <div className="grid grid-cols-12 ">
+        <div className="col-span-2 ">
           <Sidebar />
         </div>
-        <div className="col-span-10">right side</div>
+        <div
+          className="col-span-10 ml-10
+        "
+        >
+          right side
+        </div>
       </div>
     </main>
   );
