8e0305bd9f04ef2d24fd5117eaff4c355ecc004b
Sadia0920 <sadiaafrin0920@gmail.com>
2025-03-17 14:27:27 +0600

    added a lottie animation to the forget password page

--- src/pages/ForgetPasswordPage.jsx
+++ src/pages/ForgetPasswordPage.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import Lottie from "lottie-react";
+import forgetPasswordLottieData from "../assets/animation/forget-password.json"
 import { Link } from "react-router";
 
 const ForgetPasswordPage = () => {
-  return <div className="container py-8 lg:py-12">
+  return <div className="container pb-8 pt-24 lg:pb-12">
   <div className="hero bg-background">
  <div className="hero-content flex-1 flex-col-reverse lg:flex-row gap-10">
 
@@ -22,7 +24,9 @@ const ForgetPasswordPage = () => {
       </form>
      </div>
    </div>
-
+   <div className="w-9/12 md:w-7/12 lg:w-6/12">
+    <Lottie animationData={forgetPasswordLottieData}></Lottie>
+   </div>
    
  </div>
 </div>
