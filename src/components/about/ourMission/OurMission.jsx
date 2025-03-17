3a7ca706ece15c68a77d4630503456cb896fe400
fhanif16 <fatemahanif16@gmail.com>
2025-03-16 23:00:28 -0400

    our team section updated

---
+++ src/components/about/ourMission/OurMission.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const OurMission = () => {
+    return (
+       <div className="container mx-auto py-16">
+         <div className='grid grid-cols-12 gap-4 mx-auto py-16'>
+
+<div className='grid lg:col-span-9 gap-8'>
+
+    <h1>Our Mission</h1>
+    <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto totam, ut porro quis repellendus alias beatae deserunt cumque labore libero aliquid fugiat fuga eos ratione sapiente natus vel, soluta similique?</h3>
+
+
+    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur natus accusamus voluptate, nam labore, aut praesentium veritatis voluptatum iusto doloribus explicabo tenetur aspernatur? Accusamus fugiat perferendis totam quaerat sequi fuga!</h4>
+
+    
+
+</div>
+
+<div className='grid lg:col-span-3'>
+    <h1>44 million</h1> 
+    <p>Transactions every 24 hours</p>
+    <br></br>
+    <h1>44 million</h1> 
+    <p>Transactions every 24 hours</p>
+    <br></br>
+    <h1>44 million</h1> 
+    <p>Transactions every 24 hours</p>
+
+</div>
+
+</div>
+       </div>
+    );
+};
+
+export default OurMission;
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import OurTeam from "../components/about/ourTeam/OurTeam";
 import Trusted from "../components/about/trusted/Trusted";
 import ConnectingPeople from "../components/about/connectingPeople/ConnectingPeople";
+import OurMission from "../components/about/ourMission/OurMission";
 
 const AboutPage = () => {
   return (
     <div>
       <ConnectingPeople></ConnectingPeople>
+      <OurMission></OurMission>
      <OurTeam></OurTeam>
      <Trusted></Trusted>
+
     </div>
   );
 };
