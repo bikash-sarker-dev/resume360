eb718aeb165f03e096020c008f3257b6fbda40c2
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-03-18 15:08:36 +0600

    education component design done. There are 3 compunent in the edcation folder

---
+++ src/components/information-input/education/EducationList.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import EducationForm from "./EducationForm";
+import EducationCard from "./EducationCard";
+import { Button } from "@mui/material";
+
+const EducationList = () => {
+  const [educations, setEducations] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+
+  const addEducation = (newEducation) => {
+    console.log("Added Education:", newEducation);
+    setEducations([...educations, newEducation]);
+  };
+
+  return (
+    <div className="max-w-5xl mx-auto p-4">
+      {/* Button to open Modal */}
+      <div className="flex justify-end mb-6">
+        <Button variant="contained" color="primary" onClick={() => setOpenModal(true)}>
+          Add Education
+        </Button>
+      </div>
+
+      {/* Modal Form */}
+      <EducationForm
+        open={openModal}
+        handleClose={() => setOpenModal(false)}
+        addEducation={addEducation}
+      />
+
+      {/* Education Cards */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {educations.map((edu, index) => (
+          <EducationCard key={index} education={edu} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EducationList;
