c6e8a97b98deab8d4ec37f24e6449475f59cce91
fhanif16 <fatemahanif16@gmail.com>
2025-03-17 20:02:27 -0400

    trusted companies added

--- src/components/about/trusted/Trusted.jsx
+++ src/components/about/trusted/Trusted.jsx
@@ -18,12 +18,13 @@ const Trusted = () => {
   
 
   return (
-    <div className="bg-r-info-card py-24">
+   <div  className="container mx-auto py-16">
+     <div className="bg-r-info-card py-24">
         <h1 className='text-center text-xl font-bold'>Trusted by the world's most innovative teams</h1>
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
           {companies.map((company) => ( 
-              <div key={company.id} className="bg-r-info rounded-md text-center py-8 px-4 shadow-lg">
+              <div key={company.id} className=" rounded-md text-center py-8 px-4 shadow-lg">
                 <div className="flex gap-2 items-center justify-center">
                   <img
                     src={company.icon}
@@ -38,6 +39,7 @@ const Trusted = () => {
         </div>
       </div>
     </div>
+   </div>
   );
 };
 
