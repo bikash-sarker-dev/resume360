3e721f0f0d1a7c4a9e0c4c2d0d3ce2b5a425c47f
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-03-18 13:08:56 +0600

    dashboar others 2 page add order detais page or chat page

--- src/layouts/DashboardLayouts.jsx
+++ src/layouts/DashboardLayouts.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Outlet } from "react-router";
 import NavbarDashboard from "../dashboard/headNavbar/NavbarDashboard";
 import Sidebar from "../dashboard/sideBar/Sidebar";
 
@@ -6,15 +7,19 @@ const DashboardLayouts = () => {
   return (
     <main>
       <div className="grid grid-cols-12 ">
+        {/* side bar start  */}
         <div className="col-span-2 ">
           <Sidebar />
         </div>
+        {/* dashboard body part  */}
         <div
           className="col-span-10 ml-9 
         "
         >
           <NavbarDashboard />
-          <div className="p-10">right side</div>
+          <div className="p-10">
+            <Outlet />
+          </div>
         </div>
       </div>
     </main>
