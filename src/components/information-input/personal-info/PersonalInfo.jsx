95914e77606327dfedd4831100a2fe973231e402
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-03-19 21:52:42 +0600

    personal information some space and gap are added

--- src/components/information-input/personal-info/PersonalInfo.jsx
+++ src/components/information-input/personal-info/PersonalInfo.jsx
@@ -23,7 +23,7 @@ const PersonalInfo = () => {
     return (
         <>
         <h2 className="text-2xl font-bold mb-4">Add Personal Info</h2>
-            <div className='md:grid grid-cols-2 gap-4'>
+            <div className='md:grid grid-cols-2 gap-4 px-4 space-y-5'>
                 <TextField
                     className='w-full'
                     required
