3d577561c266e18ea099b5cf3c742f9dafe0f596
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-03-19 21:14:00 +0600

    socal link resmonsive done

--- src/components/information-input/social-links/SocialLinksList.jsx
+++ src/components/information-input/social-links/SocialLinksList.jsx
@@ -10,9 +10,20 @@ const SocialLinksList = () => {
   };
 
   return (
-    <div className="max-w-3xl mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-4">Social Links</h2>
-      
+    <div className="p-4">
+      <div className="flex justify-between items-center">
+        <h2 className="text-2xl font-bold mb-4">Social Links</h2>
+
+        {/* Open Modal Button */}
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="rounded-full text-white bg-r-primary py-2 px-5"
+        >
+          Add Social Link
+        </button>
+      </div>
+
+
       <div className="space-y-2">
         {socialLinks.map((social, index) => (
           <div key={index} className="border p-3 rounded shadow">
@@ -20,14 +31,6 @@ const SocialLinksList = () => {
           </div>
         ))}
       </div>
-
-      {/* Open Modal Button */}
-      <button
-        onClick={() => setIsModalOpen(true)}
-        className="mt-6 px-4 py-2 rounded-lg text-white bg-blue-600 hover:bg-blue-700"
-      >
-        Add Social Link
-      </button>
 
       {/* Modal */}
       {isModalOpen && (
