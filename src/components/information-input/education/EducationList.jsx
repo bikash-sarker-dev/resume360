d60b88c31dd8a80e1d4139ba07ae1618162e1ba1
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-03-20 14:13:15 +0600

    in educationlist add live preview section

--- src/components/information-input/education/EducationList.jsx
+++ src/components/information-input/education/EducationList.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import EducationForm from "./EducationForm";
 import EducationCard from "./EducationCard";
 import { Button } from "@mui/material";
+import SectionHead from "../../header/section-head/SectionHead";
 
 const EducationList = () => {
   const [educations, setEducations] = useState([]);
@@ -14,9 +15,11 @@ const EducationList = () => {
 
   return (
     <div className="p-4">
-      <div className="flex justify-between items-center">
-        <h2 className="text-2xl font-bold mb-4">Add Education</h2>
-
+      <SectionHead
+        subTitle={"Add your education information"}
+        title={"Add Education"}
+      />
+      <div className="flex justify-center">
         {/* Open Modal Button */}
         <button
           onClick={() => setOpenModal(true)}
@@ -34,10 +37,16 @@ const EducationList = () => {
       />
 
       {/* Education Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {educations.map((edu, index) => (
-          <EducationCard key={index} education={edu} />
-        ))}
+      <div className="grid grid-cols-2">
+        <div className="grid grid-cols-2 gap-4">
+          {educations.map((edu, index) => (
+            <EducationCard className="h-fit" key={index} education={edu} />
+          ))}
+        </div>
+        <div>
+          
+          <img src="https://i.ibb.co.com/pBTDf678/Whats-App-Image-2025-03-20-at-13-20-15-8709966e.jpg" alt="resume" />
+        </div>
       </div>
     </div>
   );
