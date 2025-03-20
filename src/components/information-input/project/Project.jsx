2cb1544b3a3f3f5c3f1b94a7c7d9ccce84eaa6bc
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-03-20 14:56:01 +0600

    add project compunent and in stepper compunent add new stepper

---
+++ src/components/information-input/project/Project.jsx
@@ -0,0 +1,14 @@
+import SectionHead from "../../header/section-head/SectionHead";
+
+const Project = () => {
+    return (
+        <>
+            <SectionHead
+                subTitle={"Add your project"}
+                title={"Add Project"}
+            />
+        </>
+    );
+};
+
+export default Project;
