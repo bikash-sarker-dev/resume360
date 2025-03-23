2f0f2b2b3db42b30671bda8528ffad66f19a3c1d
fhanif16 <fatemahanif16@gmail.com>
2025-03-23 12:23:34 -0400

    dashboard updated

--- src/pages/DashboardPage/DashboardUserPage.jsx
+++ src/pages/DashboardPage/DashboardUserPage.jsx
@@ -5,17 +5,7 @@ import UserDashboard from "../../dashboard/userDashboard/userDashboard/UserDashb
 const DashboardUserPage = () => {
   return <div>
 
-   {/* <div className= "grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-   <TotalUserRegistered></TotalUserRegistered>
-   <TotalUsewrLoggedIn></TotalUsewrLoggedIn>
-   </div>
-   <div className= "grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-   <TotalResumeUsed></TotalResumeUsed>
-   <ProfitChart></ProfitChart>
-   </div>
-   <div className="flex justify-center items-center">
-        <DownloadTracker />
-      </div> */}
+ 
 
       <UserDashboard></UserDashboard>
   </div>;
