fb207359a2fa77fd8d138e90757e5b4fd71a6f40
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-03-24 19:41:00 +0600

    education design formate is update

--- src/components/information-input/education/EducationList.jsx
+++ src/components/information-input/education/EducationList.jsx
@@ -1,13 +1,11 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import EducationForm from "./EducationForm";
-import EducationCard from "./EducationCard";
 import SectionHead from "../../header/section-head/SectionHead";
 import LivePreview from "../live-preview/LivePreview";
 import { ResumeContext } from "../../../contextApi/resume-context/ResumeContext";
 
 const EducationList = () => {
   const { resumeData, updateSection } = useContext(ResumeContext);
-  const [openModal, setOpenModal] = useState(false);
 
   const addEducation = (newEducation) => {
     const updatedEducation = [...resumeData.education, newEducation];
@@ -20,32 +18,11 @@ const EducationList = () => {
         subTitle={"Add your education information"}
         title={"Add Education"}
       />
-
-      <div className="flex justify-center">
-        <button
-          onClick={() => setOpenModal(true)}
-          className="rounded-full text-white bg-r-primary py-2 px-5"
-        >
-          Add Education
-        </button>
-      </div>
-
-      {/* Modal Form */}
-      <EducationForm
-        open={openModal}
-        handleClose={() => setOpenModal(false)}
-        addEducation={addEducation}
-      />
-
       {/* Education Cards */}
       <div className="grid grid-cols-2">
-        <div className="grid grid-cols-2 gap-4">
-          {resumeData.education.map((edu, index) => (
-            <EducationCard className="h-fit" key={index} education={edu} />
-          ))}
-        </div>
-
-        {/* Live Preview */}
+        <EducationForm
+          addEducation={addEducation}
+        />
         <div>
           <LivePreview />
         </div>
