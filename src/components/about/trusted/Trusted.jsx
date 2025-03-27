10afaec3f6403bfaec3c9a643ad5f669efef528e
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-03-27 15:50:44 +0600

    the about page working change marquee barnd added

--- src/components/about/trusted/Trusted.jsx
+++ src/components/about/trusted/Trusted.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import Marquee from "react-fast-marquee";
+import React, { useEffect, useState } from "react";
+import BrandLogo from "../../home/brandLogo/BrandLogo";
 
 const Trusted = () => {
   const [companies, setCompanies] = useState([]);
@@ -8,47 +8,17 @@ const Trusted = () => {
     fetch("trusted.json")
       .then((res) => res.json())
       .then((data) => {
-      
-          setCompanies(data.companies);
-       
-      })
-      
+        setCompanies(data.companies);
+      });
   }, []);
 
-
-  
-
   return (
-   <div  className=" mx-auto py-16">
-    
-     <div className="bg-r-info-card py-24">
-        <h1 className='text-center text-2xl font-bold'>Trusted by the world's most innovative teams</h1>
-      <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-          {companies.map((company) => ( 
-              <div key={company.id} className=" rounded-md text-center py-8 px-4 shadow-lg">
-
-                
-
-                <Marquee > <div className="flex gap-2 items-center justify-center">
-                  <img
-                    src={company.icon}
-                    alt={company.name}
-                    className="h-16 w-16 object-contain"
-                    loading="lazy"
-                  />
-                  <h1 className="text-2xl font-bold">{company.name}</h1>
-                </div> </Marquee>
-              </div>
-          ))}
-        </div>
+    <div className="bg-r-info-card">
+      <div className="container">
+        <BrandLogo />
       </div>
     </div>
-   </div>
   );
 };
 
 export default Trusted;
-
-
-
