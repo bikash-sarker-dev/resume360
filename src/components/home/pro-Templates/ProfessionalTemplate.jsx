f511fa3924294da84f6f4e5e28001a8bb36cd5a4
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-03-27 11:54:28 +0600

    the home page professional template slide change

--- src/components/home/pro-Templates/ProfessionalTemplate.jsx
+++ src/components/home/pro-Templates/ProfessionalTemplate.jsx
@@ -4,13 +4,11 @@ import resume2 from "../../../assets/images/resume/resume2.png";
 import resume3 from "../../../assets/images/resume/resume3.png";
 import resume4 from "../../../assets/images/resume/resume4.png";
 import resume5 from "../../../assets/images/resume/resume5.png";
-import ButtonTemplate from "../pro-Templates/ButtonTemplate"
-import  SectionHead from "../../../components/header/section-head/SectionHead"
-
-
+import SectionHead from "../../../components/header/section-head/SectionHead";
+import ButtonTemplate from "../pro-Templates/ButtonTemplate";
 
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
@@ -54,69 +52,66 @@ const ProfessionalTemplate = () => {
           handleCv={handleCv}
           handleCoverLetter={handleCoverLetter}
         />
-      
       </div>
-      <div className="mt-10 mx-5 ">
-        {resume && (
-          <div>
-            <Swiper
-              slidesPerView={3}
-              breakpoints={{
-                768: { slidesPerView: 2 }, 
-                0: { slidesPerView: 1 }, 
-              }}
-              spaceBetween={30}
-              freeMode={true}
-              autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-              }}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Autoplay, FreeMode, Pagination]}
-              className="mySwiper"
-            >
-              <SwiperSlide>
-                <div className="p-5 bg-r-primary/25 border border-r-primary backdrop-blur-lg rounded-xl">
-                  <img src={resume1} alt="resume image one" />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="p-5 bg-r-primary/25 border border-r-primary backdrop-blur-lg rounded-xl">
-                  <img src={resume2} alt="resume image one" />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="p-5 bg-r-primary/25 border border-r-primary backdrop-blur-lg rounded-xl">
-                  <img src={resume3} alt="resume image one" />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="p-5 bg-r-primary/25 border border-r-primary backdrop-blur-lg rounded-xl">
-                  <img src={resume4} alt="resume image one" />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="p-5 bg-r-primary/25 border border-r-primary backdrop-blur-lg rounded-xl">
-                  <img src={resume5} alt="resume image one" />
-                </div>
-              </SwiperSlide>
-            </Swiper>
-          </div>
-        )}
-        {cv && (
-          <div>
-            <h1 className="text-5xl text-center mt-16 text-orange-400">Cv</h1>
-          </div>
-        )}
-        {coverLetter && (
-          <div>
-            <h1 className="text-5xl text-center mt-16 text-orange-400">
-              Cover letter
-            </h1>
-          </div>
-        )}
+      <div className="container">
+        <div className="mt-10 mx-5 ">
+          {resume && (
+            <div>
+              <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                freeMode={true}
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[Autoplay, FreeMode, Pagination]}
+                className="mySwiper"
+              >
+                <SwiperSlide>
+                  <div className="">
+                    <img src={resume1} alt="resume image one" />
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="">
+                    <img src={resume2} alt="resume image one" />
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="">
+                    <img src={resume3} alt="resume image one" />
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="">
+                    <img src={resume4} alt="resume image one" />
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="">
+                    <img src={resume5} alt="resume image one" />
+                  </div>
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          )}
+          {cv && (
+            <div>
+              <h1 className="text-5xl text-center mt-16 text-orange-400">Cv</h1>
+            </div>
+          )}
+          {coverLetter && (
+            <div>
+              <h1 className="text-5xl text-center mt-16 text-orange-400">
+                Cover letter
+              </h1>
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
