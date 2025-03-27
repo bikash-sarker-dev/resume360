0cfdc296e761b75184e875499519d4775574e4b3
fhanif16 <fatemahanif16@gmail.com>
2025-03-27 09:39:26 -0400

    contact info page added

--- src/components/contact/ContactInfo.jsx
+++ src/components/contact/ContactInfo.jsx
@@ -4,7 +4,7 @@ import Marquee from 'react-fast-marquee';
 const ContactInfo = () => {
     return (
         <div className="container mx-auto py-16 px-4 md:px-8">
-            {/* <div className="text-center space-y-6">
+            <div className="text-center space-y-6">
                 <h2 className="text-3xl sm:text-4xl md:text-5xl text-r-text font-bold mt-1">
                     Need Assistance? Our Awesome Team is Here to Support You – Feel Free to Reach Out!
                 </h2>
@@ -12,12 +12,12 @@ const ContactInfo = () => {
                     We're Just a Message Away – Let Us Help You!
                 </h4>
                 
-                <Marquee speed={40} gradient={false} className=" py-4 rounded-lg shadow-lg">
+                {/* <Marquee speed={40} gradient={false} className=" py-4 rounded-lg shadow-lg">
                     <h2 className="text-lg sm:text-xl md:text-2xl text-r-text font-bold mt-1">
                         Our Support Team is Available 24/7 – We’re Always Ready to Help! For Urgent Assistance, Call Us at (347)-748-3331.
                     </h2>
-                </Marquee>
-            </div> */}
+                </Marquee> */}
+            </div>
         </div>
     );
 };
