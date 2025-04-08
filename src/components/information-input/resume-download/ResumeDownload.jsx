a9d27b861847cd58ddfa3995010e7e21e4535225
adnanmahmud0 <adnan.mahmud.pc@gmail.com>
2025-04-08 17:05:08 +0600

    pdf and word download setup done

--- src/components/information-input/resume-download/ResumeDownload.jsx
+++ src/components/information-input/resume-download/ResumeDownload.jsx
@@ -1,12 +1,29 @@
-
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import ResumePDF from './ResumePDF';
+import { generateDocx } from './generateDocx';
+import { useContext } from 'react';
+import { ResumeContext } from '../../../contextApi/resume-context/ResumeContext';
 
 
 const ResumeDownload = () => {
-
+    const { resumeData } = useContext(ResumeContext);
     return (
-        <>
-            <button className="btn">Resume Download</button>
-        </>
+        <div className="flex gap-4 mt-4">
+            <PDFDownloadLink
+                document={<ResumePDF resumeData={resumeData} />}
+                fileName={`${resumeData?.personalInfo?.fullName || 'resume'}.pdf`}
+                className="btn"
+            >
+                {({ loading }) => (loading ? 'Preparing PDF...' : 'Download PDF')}
+            </PDFDownloadLink>
+
+            <button
+                onClick={() => generateDocx(resumeData)}
+                className="btn bg-blue-500 text-white rounded px-4 py-2"
+            >
+                Download DOCX
+            </button>
+        </div>
     );
 };
 
