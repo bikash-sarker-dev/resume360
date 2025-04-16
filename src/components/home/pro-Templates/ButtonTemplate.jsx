2602431bf06aab109cb347a301e4604be751ae2b
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-04-17 00:42:37 +0600

    added active button status in templatesPage

--- src/components/home/pro-Templates/ButtonTemplate.jsx
+++ src/components/home/pro-Templates/ButtonTemplate.jsx
@@ -24,7 +24,9 @@ const ButtonTemplate = ({
         onClick={handleCv}
         className={` ${
           cv && "bg-r-primary"
-        } relative inline-flex items-center justify-center px-8 py-3 overflow-hidden  font-bold  text-r-text bg-r-background rounded-lg group slide-up slide-up-btn`}
+        } relative inline-flex items-center justify-center
+         px-8 py-3 overflow-hidden  font-bold
+           text-r-text bg-r-background rounded-lg group slide-up slide-up-btn`}
       >
         <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-r-primary rounded-full group-hover:w-56 group-hover:h-56"></span>
         <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-r-primary "></span>
