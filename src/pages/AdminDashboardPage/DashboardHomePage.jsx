3548d8f9e7a3b6cb415351e1e9744fcf24f76e70
Sadia0920 <sadiaafrin0920@gmail.com>
2025-04-16 13:20:39 +0600

    create routes for user dashboard

---
+++ src/pages/AdminDashboardPage/DashboardHomePage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ColumnCharts from "../../dashboard/HomeDashboard/charts/ColumnCharts";
+import PieCharts from "../../dashboard/HomeDashboard/charts/PieCharts";
+import StatusCard from "../../dashboard/HomeDashboard/homeCard/StatusCard";
+
+const DashboardHomePage = () => {
+  return (
+    <div>
+      <StatusCard />
+      <div className="lg:flex gap-10 mt-5 p-5">
+        <div className="lg:w-2/3">
+          <ColumnCharts />
+        </div>
+        <div className="lg:w-1/3">
+          <PieCharts />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardHomePage;
