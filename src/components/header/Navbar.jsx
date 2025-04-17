a889563f4451c3b9010096151bdc827c0e0fa99f
kazi tareq <kazitareq321@gmail.com>
2025-04-17 13:44:59 +0600

    login and registration active btn and banner height

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -150,7 +150,9 @@ const Navbar = () => {
                           to="/user"
                           className={({ isActive }) =>
                             `cursor-pointer text-lg w-max transition-all hover:text-r-hover ${
-                              isActive ? 'border-b border-r-background font-semibold' : 'text-r-background'
+                              isActive
+                                ? "border-b border-r-background font-semibold"
+                                : "text-r-background"
                             }`
                           }
                         >
@@ -193,28 +195,41 @@ const Navbar = () => {
               <>
                 {/* Login Button (Desktop) */}
                 <div className="flex gap-5">
-                  <Link to="/login">
-                    <button className="relative  hidden lg:flex   px-4 py-2 text-[17px] font-semibold text-text border border-r-card rounded-full overflow-hidden transition-colors duration-300 ease-out group hover:text-r-text hover:border-transparent">
-                      <div className="flex items-center">
-                        <span className="absolute top-1/2 left-1/2 w-[20em] h-[20em] -translate-x-1/2 -translate-y-1/2 rounded-full z-[-1] transition-[box-shadow] duration-500 ease-out group-hover:shadow-[inset_0_0_0_10em_var(--color-r-accent)]" />
+                  <NavLink
+                    to="/login"
+                    className={({ isActive }) =>
+                      `relative hidden lg:flex px-4 py-2 text-[17px] font-semibold border rounded-full overflow-hidden transition-colors duration-300 ease-out group
+                ${
+                  isActive
+                    ? "text-r-text bg-[var(--color-r-accent)] border-transparent"
+                    : "text-text border-r-card hover:text-r-text hover:border-transparent"
+                }`
+                    }
+                  >
+                    <div className="flex items-center">
+                      <span className="absolute top-1/2 left-1/2 w-[20em] h-[20em] -translate-x-1/2 -translate-y-1/2 rounded-full z-[-1] transition-[box-shadow] duration-500 ease-out group-hover:shadow-[inset_0_0_0_10em_var(--color-r-accent)]" />
+                      <FaUserLock className="mr-3" />
+                      <span>login</span>
+                    </div>
+                  </NavLink>
 
-                        <FaUserLock className="mr-3"></FaUserLock>
-
-                        <span>login</span>
-                      </div>
-                    </button>
-                  </Link>
-                  <Link to="/register">
-                    <button className="relative  hidden lg:flex   px-4 py-2 text-[17px] font-semibold text-text border border-r-card rounded-full overflow-hidden transition-colors duration-300 ease-out group hover:text-r-text hover:border-transparent">
-                      <div className="flex items-center">
-                        <span className="absolute top-1/2 left-1/2 w-[20em] h-[20em] -translate-x-1/2 -translate-y-1/2 rounded-full z-[-1] transition-[box-shadow] duration-500 ease-out group-hover:shadow-[inset_0_0_0_10em_var(--color-r-accent)]" />
-
-                        <FaUserLock className="mr-3"></FaUserLock>
-
-                        <span>register</span>
-                      </div>
-                    </button>
-                  </Link>
+                  <NavLink
+                    to="/register"
+                    className={({ isActive }) =>
+                      `relative hidden lg:flex px-4 py-2 text-[17px] font-semibold border rounded-full overflow-hidden transition-colors duration-300 ease-out group
+                 ${
+                   isActive
+                     ? "text-r-text bg-[var(--color-r-accent)] border-transparent"
+                     : "text-text border-r-card hover:text-r-text hover:border-transparent"
+                 }`
+                    }
+                  >
+                    <div className="flex items-center">
+                      <span className="absolute top-1/2 left-1/2 w-[20em] h-[20em] -translate-x-1/2 -translate-y-1/2 rounded-full z-[-1] transition-[box-shadow] duration-500 ease-out group-hover:shadow-[inset_0_0_0_10em_var(--color-r-accent)]" />
+                      <FaUserLock className="mr-3" />
+                      <span>register</span>
+                    </div>
+                  </NavLink>
                 </div>
               </>
             )}
@@ -299,28 +314,41 @@ const Navbar = () => {
                 onClick={() => setMenuOpen(false)}
                 className="flex gap-5 justify-center mb-5"
               >
-                <Link to="/login">
-                  <button className="relative  flex md:hidden   px-4 py-2 text-[17px] font-semibold text-r-hover border border-r-card rounded-full overflow-hidden transition-colors duration-300 ease-out group hover:text-r-text hover:border-transparent">
-                    <div className="flex items-center">
-                      <span className="absolute top-1/2 left-1/2 w-[20em] h-[20em] -translate-x-1/2 -translate-y-1/2 rounded-full z-[-1] transition-[box-shadow] duration-500 ease-out group-hover:shadow-[inset_0_0_0_10em_var(--color-r-accent)]" />
+                <NavLink
+                  to="/login"
+                  className={({ isActive }) =>
+                    `relative flex md:hidden px-4 py-2 text-[17px] font-semibold border rounded-full overflow-hidden transition-colors duration-300 ease-out group
+                 ${
+                   isActive
+                     ? " bg-[var(--color-r-accent)] border-transparent"
+                     : "text-r-hover border border-r-card hover:text-r-text hover:border-transparent"
+                 }`
+                  }
+                >
+                  <div className="flex items-center">
+                    <span className="absolute top-1/2 left-1/2 w-[20em] h-[20em] -translate-x-1/2 -translate-y-1/2 rounded-full z-[-1] transition-[box-shadow] duration-500 ease-out group-hover:shadow-[inset_0_0_0_10em_var(--color-r-accent)]" />
+                    <FaUserLock className="mr-3" />
+                    <span>login</span>
+                  </div>
+                </NavLink>
 
-                      <FaUserLock className="mr-3"></FaUserLock>
-
-                      <span>login</span>
-                    </div>
-                  </button>
-                </Link>
-                <Link to="/register">
-                  <button className="relative  flex md:hidden  px-4 py-2 text-[17px] font-semibold text-r-hover border border-r-card rounded-full overflow-hidden transition-colors duration-300 ease-out group hover:text-r-text hover:border-transparent">
-                    <div className="flex items-center">
-                      <span className="absolute top-1/2 left-1/2 w-[20em] h-[20em] -translate-x-1/2 -translate-y-1/2 rounded-full z-[-1] transition-[box-shadow] duration-500 ease-out group-hover:shadow-[inset_0_0_0_10em_var(--color-r-accent)]" />
-
-                      <FaUserLock className="mr-3"></FaUserLock>
-
-                      <span>register</span>
-                    </div>
-                  </button>
-                </Link>
+                <NavLink
+                  to="/register"
+                  className={({ isActive }) =>
+                    `relative flex md:hidden px-4 py-2 text-[17px] font-semibold border rounded-full overflow-hidden transition-colors duration-300 ease-out group
+                  ${
+                    isActive
+                      ? " bg-[var(--color-r-accent)]  border-transparent"
+                      : "text-r-hover border border-r-card hover:text-r-text hover:border-transparent"
+                  }`
+                  }
+                >
+                  <div className="flex items-center">
+                    <span className="absolute top-1/2 left-1/2 w-[20em] h-[20em] -translate-x-1/2 -translate-y-1/2 rounded-full z-[-1] transition-[box-shadow] duration-500 ease-out group-hover:shadow-[inset_0_0_0_10em_var(--color-r-accent)]" />
+                    <FaUserLock className="mr-3" />
+                    <span>register</span>
+                  </div>
+                </NavLink>
               </div>
             </>
           )}
