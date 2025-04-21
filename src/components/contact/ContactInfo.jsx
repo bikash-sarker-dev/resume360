d9f10835f139b9368300c495432f1fab5b1b7b90
Sadia0920 <sadiaafrin0920@gmail.com>
2025-04-21 15:05:24 +0600

    polish a terms page

--- src/components/contact/ContactInfo.jsx
+++ src/components/contact/ContactInfo.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
+import SectionHead from '../header/section-head/SectionHead';
 
 const ContactInfo = () => {
     return (
-        <div className="container mx-auto py-16 px-4 md:px-8">
+        <div className="w-full mx-auto py-6">
             <div className="text-center space-y-4">
-                <h2 className="text-3xl sm:text-4xl md:text-5xl text-r-text font-bold mt-1">
-                    Need Assistance? 
-                   
-                </h2>
-                <h4 className="text-lg sm:text-xl md:text-2xl text-r-text opacity-70">
-                Our Awesome Team is Here to Support You Feel Free to Reach Out!
-                   
-                </h4>
-                
+                <SectionHead title={"Need Assistance? "}></SectionHead>
+                <h4 className="text-xl text-r-text opacity-50">
+                Our Awesome Team is Here to Support You Feel Free to Reach Out!</h4>
                 <Marquee speed={40} gradient={false} className=" py-2 rounded-lg shadow-lg">
                     <h2 className="text-lg sm:text-xl md:text-2xl text-r-text font-bold mt-1">
                         Our Support Team is Available 24/7 – We’re Always Ready to Help! For Urgent Assistance, Call Us at (347)-748-3331.
