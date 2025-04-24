fd72939dfe8281c24fa025f4ada398dd4dbd236d
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-04-25 00:10:48 +0600

    added link to the social icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -68,15 +68,18 @@ const Footer = () => {
             </h3>
 
             <div className="mt-4 flex justify-center md:justify-start space-x-4">
-              <a className=" p-1 bg-[#1877F2]
+              <a
+              href="https://www.facebook.com/tamjid.razin/"
+              target="_blank"
+              className=" p-1 bg-[#1877F2]
  text-white rounded-md cursor-pointer text-2xl">
                 <FaFacebookF />
               </a>
               
-              <a className=" p-1 bg-[#0077B5] text-white rounded-md cursor-pointer text-2xl">
+              <a href="https://www.linkedin.com/in/sadia-afrin-597515301/" target="_blank" className=" p-1 bg-[#0077B5] text-white rounded-md cursor-pointer text-2xl">
                 <FaLinkedinIn />
               </a>
-              <a className=" p-1 bg-[#5865F2] text-white rounded-md cursor-pointer text-2xl">
+              <a href="https://discord.gg/FNbcjMhZ" target="_blank" className=" p-1 bg-[#5865F2] text-white rounded-md cursor-pointer text-2xl">
                 < FaDiscord />
               </a>
             </div>
