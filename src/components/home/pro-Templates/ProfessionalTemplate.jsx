2d3b1948531693b7ef9d423117160e94e4f872b9
Sadia0920 <sadiaafrin0920@gmail.com>
2025-04-29 14:41:50 +0600

    change the pagination defatul color of CV and Cover Letters in professional templates section

--- src/components/home/pro-Templates/ProfessionalTemplate.jsx
+++ src/components/home/pro-Templates/ProfessionalTemplate.jsx
@@ -127,6 +127,10 @@ const ProfessionalTemplate = () => {
           )}
           {cv && (
             <Swiper
+            style={{
+              "--swiper-navigation-color": "var(--color-r-info)",
+              "--swiper-pagination-color": "var(--color-r-accent)",
+            }}
               slidesPerView={3}
               spaceBetween={20}
               freeMode={true}
@@ -202,6 +206,10 @@ const ProfessionalTemplate = () => {
           )}
           {coverLetter && (
             <Swiper
+            style={{
+              "--swiper-navigation-color": "var(--color-r-info)",
+              "--swiper-pagination-color": "var(--color-r-accent)",
+            }}
               slidesPerView={3}
               spaceBetween={20}
               freeMode={true}
